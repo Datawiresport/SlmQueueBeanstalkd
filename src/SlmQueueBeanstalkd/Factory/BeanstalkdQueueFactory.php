5354a88296b6ac204131df725e31ac34abc00b91
Leander Philippo <lphilippo@adventive.es>
2020-05-30 22:09:06 +0200

    Replace container

--- src/SlmQueueBeanstalkd/Factory/BeanstalkdQueueFactory.php
+++ src/SlmQueueBeanstalkd/Factory/BeanstalkdQueueFactory.php
@@ -22,11 +22,10 @@ class BeanstalkdQueueFactory implements FactoryInterface
      */
     public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
     {
-        $serviceManager = $container->getServiceManager();
-        $pheanstalk = $serviceManager->get('SlmQueueBeanstalkd\Service\PheanstalkService');
-        $jobPluginManager = $serviceManager->get('SlmQueue\Job\JobPluginManager');
+        $pheanstalk = $container->get('SlmQueueBeanstalkd\Service\PheanstalkService');
+        $jobPluginManager = $container->get('SlmQueue\Job\JobPluginManager');
 
-        $queueOptions = $this->getQueueOptions($serviceManager, $requestedName);
+        $queueOptions = $this->getQueueOptions($container, $requestedName);
 
         return new BeanstalkdQueue($pheanstalk, $requestedName, $jobPluginManager, $queueOptions);
     }
@@ -34,14 +33,14 @@ class BeanstalkdQueueFactory implements FactoryInterface
     /**
      * Returns custom beanstalkd options for specified queue
      *
-     * @param ServiceManager $serviceManager
+     * @param ContainerInterface $container
      * @param string $queueName
      *
      * @return QueueOptions
      */
-    protected function getQueueOptions(ServiceManager $serviceManager, $queueName)
+    protected function getQueueOptions(ContainerInterface $container, $queueName)
     {
-        $config = $serviceManager->get('Config');
+        $config = $container->get('Config');
         $queuesOptions = isset($config['slm_queue']['queues'])? $config['slm_queue']['queues'] : array();
         $queueOptions = isset($queuesOptions[$queueName])? $queuesOptions[$queueName] : array();
 
